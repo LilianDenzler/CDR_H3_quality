8ca94039778debb6a669959d47a4133eb828c533
LilianDenzler <lilian_denzler@protonmail.com>
2020-10-21 21:36:18 +0200

    updated version

---
+++ parser6.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+#do chmod +x name_of_script before running
+
+#import atomium
+import os
+
+light=[]
+heavy=[]
+
+def one_letter_code(residue):
+	dic = {'CYS': 'C', 'ASP': 'D', 'SER': 'S', 'GLN': 'Q', 'LYS': 'K',
+     'ILE': 'I', 'PRO': 'P', 'THR': 'T', 'PHE': 'F', 'ASN': 'N',
+     'GLY': 'G', 'HIS': 'H', 'LEU': 'L', 'ARG': 'R', 'TRP': 'W',
+     'ALA': 'A', 'VAL':'V', 'GLU': 'E', 'TYR': 'Y', 'MET': 'M','XAA': 'X', 'UNK':'X'}
+	#one_letter= atomium.structures.code()    #####doesnt work -> why?
+	if len(residue) % 3 != 0:
+		print(residue)
+		raise ValueError("error")
+	one_letter= dic[residue]
+	return (one_letter)
+
+def parser(position,residue,chain_type):
+	global string
+	for i in zip(position, residue, chain_type):
+		one_res=one_letter_code(residue)
+		string+= '{}{}\t{}\n'.format(chain_type,position,one_res)
+	return (string)
+
+
+counter=0
+directory = os.fsencode('/home/lilian/Desktop/structures/LH_Combined_Chothia')
+for filename in os.listdir(directory):
+	string=[]
+	file=os.fsdecode(filename)
+	if file.endswith(".pdb")== False:
+		print("this file is not in correct format (i.e. not PDB){}".format(file))
+		continue
+	with open("/home/lilian/Desktop/structures/LH_Combined_Chothia/{}".format(file), "r") as file2:
+		for line in file2:
+			if "ATOM" in line and "CA" in line:
+				fields = line.strip().split()
+				pos= fields[5]
+				res= fields[3]
+				chain= fields [4]
+				string= parser(pos, res, chain)
+	file2.close()
+
+	new_input_file= open("/home/lilian/Desktop/structures/input_Abymod_seq/{}".format(file),"w")
+	for i in string:
+		new_input_file.write(i)
+	counter+=1
+	print(counter)
